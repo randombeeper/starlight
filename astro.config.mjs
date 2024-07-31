fce8272304ad36da0677ee15bdb44c1a08b74246
James Gray <randombeeper@gmail.com>
2024-07-31 12:05:20 -0600

    canonical?

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
 	site: 'https://randombeeper.github.io/',
 	base: 'starlight',
-	trailingSlash: 'never',
+	trailingSlash: 'always',
 	integrations: [
 		starlight({
 			title: 'James Astro Testing',
